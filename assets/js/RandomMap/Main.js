35472927a1daa2e7bea807d4f6d05cd7f0aea3ac
Chase Wex <clapingmanly@hotmail.com>
2020-01-21 23:47:32 -0500

    Added 2 new functional steps to the formula (add highpoints and create Elevation)

--- assets/js/RandomMap/Main.js
+++ assets/js/RandomMap/Main.js
@@ -35,22 +35,23 @@ class RandomMap
         this.stepId = 0; //id 0 means that we are starting from the begining of the cycles
         this.status = "Ready";
         this.bounds = new Rect(0,0,1,1); //dont set up bounds until we get a canvas
+        this.Seeds = { "Map": 0, "Var": 0, "Evol": 0}
         this.props =  // list of props given to the user, set to default (obviously);
         {//min default max
 
             "Seed Properties": [], //create a header
-            "Seed":     [-99999, 0, 99999],
-            "Variant":  [-99999, 0, 99999],
-            "Map Seed": [-99999, 0, 99999],
+            "Map Seed":     [-99999, 0, 99999],
+            "Variant":  [-99999, Math.random()*9999, 99999],
+            "Evolution Seed": [-99999, 0, 99999],
 
             "Map Properties": [], //create a header
             "Water Height": [0.0001, 0.3, 1.0],
-            "Perlin Weight": [0.0, 0.32, 1.0], //set this slightly above sea level so we get some islands randomly sprinkled
-            "Center Weight": [0.0, 0.68, 1.0], //this and the above must equal 1.0 Done this way so you can add more methods as weights
+            "Perlin Weight": [0.0, 0, 1.0], //set this slightly above sea level so we get some islands randomly sprinkled
+            "Center Weight": [0.0, 1.0, 1.0], //this and the above must equal 1.0 Done this way so you can add more methods as weights
             "Coast Clean Irrations": [1,5,20],
-            "Cell Percentage": [0.0001, 1.6, 3],
+            "Cell Percentage": [0.0001, 0.16, 3],
             "Min Distance": [0.00001, 5, 10],
-            "CoastalRoughness": [0.0001, 1.0, 5.0],
+            "Coastal Roughness": [0.0001, 1.0, 5.0],
             "Normalizations Cycles": [-1, 3, 5],
             "Lake Moisture": [0.0, 0.8, 1.0],
             "Moisture Threshold": [0.000001, 0.01, 0.2],
@@ -61,11 +62,11 @@ class RandomMap
             "PATEL Irrations": [0, 4, 10],
 
             "Heigh Point Properties": [],
-            "Number of High Points": [1,3],
-            "X Range": [500, 2500],
-            "Y Range": [500, 2500],
-            "Elevation Range": [0.6, 1.0],
-            "Elevation Dropoff": [0.8, 1.8]
+            "Number of High Points": [6,20],
+            "X Range": [1000, 1000],
+            "Y Range": [1000, 1000],
+            "Elevation Range": [0.8, 0.4],
+            "Elevation Dropoff": [1.5, 1.5]
         }
 
         //this.init();
@@ -103,8 +104,6 @@ class RandomMap
         e, //of type utils.edge
         gedge, gedges= this.diagram.edges, gcells = this.diagram.cells,  //of type voronoi.edge
         iCell=gcells.length, iEdge=gedges.length, site;
-        //console.log(this.diagram)
-        //return false;
         while(iCell--) cells.push(new Cell({x: -100000, y: -100000}, iCell));
         while(iEdge--)
         {
@@ -117,14 +116,15 @@ class RandomMap
                 c1 = cells[site.voronoiId];
                 c1.center.x = site.x;
                 c1.center.y = site.y;
-            } else c1 = new Cell({x: -100000, y: -10000}, -1);//should nv get here cuz algorithm only uses lsite as null
+            } else c1 = new Cell({x: 0, y: 0}, -1);//should nv get here cuz algorithm only uses lsite as null
             site = gedge.rSite;
             if(site)
             {
                 c2 = cells[site.voronoiId];
                 c2.center.x = site.x;
                 c2.center.y = site.y;
-            }else c2 = new Cell({x: -100000, y: -10000}, -1); //creates fake cell for borders so that we create an edges
+            }else c2 = new Cell({x: 0, y: 0}, -1); //creates fake cell for borders so that we create an edge
+            //fake cells always have id -1 so we can easily detect them and not render them
 
             e = new Edge(gedges[iEdge].vb, gedges[iEdge].va, c1, c2, iEdge, this.graph.corners);
             this.graph.edges.push(e); //takes care of doing the edges;
@@ -144,9 +144,9 @@ class RandomMap
         // all data is held into the class data pool and is passed between functions
         // at the end of each cycle a delay is called to allow for the canvas to render
         // this will create a animation which will help people see how the algorithm builds
-        console.log(this.funcStep);
         if(this[this.funcStep]() == false)
         {
+            console.log(this.funcStep)
             this.dataStack = {}; //reset data stack so we dont have collisions
             this.funcStep = STEP_FUNC[this.funcStep]; //stack the next step into the slot for next cycle
         }
@@ -160,7 +160,9 @@ class RandomMap
         this.bounds = new Rect(0,0,canvas.width, canvas.height);
         this.BuildCustomGraph();
         RandomMapRender.init(canvas, this.graph);
-        
+        this.Seeds.Map = new Random(this.props["Map Seed"][1]);
+        this.Seeds.Var = new Random(this.props["Variant"][1]);
+        this.Seeds.Evol = new Random(this.props["Evolution Seed"][1]);
         return false;
     }
     GenerateSites()
@@ -169,7 +171,8 @@ class RandomMap
         let height = this.bounds.height() - margin*2;
         let width = this.bounds.width() - margin*2;
         let sitenumber = Math.max((width * height * this.props["Cell Percentage"][1]* 0.01),3);
-        let irr = Math.min(sitenumber-this.graph.sites.length,100); //only do 100 at a time
+        let irr = Math.min(sitenumber-this.graph.sites.length,500); //only do 100 at a time
+        let random = this.Seeds.Map;
         for(let i = 0; i < irr; i++)
         {
             this.graph.sites.push(new Vec2(random.randomRange(this.bounds.minX+margin, this.bounds.maxX-margin),random.randomRange(this.bounds.minY+margin, this.bounds.maxY-margin)));
@@ -185,6 +188,7 @@ class RandomMap
     { // most of this function is a direct pull from raymond hills work with small modifications to work with this system
         //Found minor bugs so needed a way to patch them and allow user more flexiblity with not fully relaxing graph if desired.
         let cells = this.diagram.cells,
+        random = this.Seeds.Map, //set this to the map seed random
         iCell = cells.length,
         cell,
         site, sites = [],
@@ -289,18 +293,147 @@ class RandomMap
             newpos[iCorner] = p1;
             
         }
-        iCorner = this.graph.corners.len;
+        iCorner = this.graph.corners.length;
         while(iCorner--) this.graph.corners[iCorner].position = newpos[iCorner];
         //keep going till we reach the amount defined (keep in mind false moves on true keeps going)
         return this.props["PATEL Irrations"][1] > this.dataStack.irrations; 
     }
-    HeightGeneration()
+    HeightGeneration() //starting here we will use Varient random for everything
     {
-       return false;
+        //here lets start defining things on the stack that we want saved. We will save these to the graph data so they stay around
+        let random = this.Seeds.Var;
+        this.graph = //we add the meta data to the graph here we mayneed to move this
+        {
+            ...this.graph,
+            LandCorners: [],
+            LakeCorners: [],
+            OceanCorners: [],
+            CoastCorners: [],
+            HighCorners: []
+        }
+        
+        this.dataStack.highpoints = [];
+        this.dataStack.irr = 1;
+
+        //what we did do was build a new voronoi graph and then set our high points based on that. This would be more complex so for now we are just gonna go pure random 
+        let numpts = this.props["Number of High Points"];
+        let irr = Math.ceil(random.randomRange(Math.min(numpts[0], numpts[1]), Math.max(numpts[0], numpts[1]))),
+        bounds = new Rect(
+            this.bounds.minX + this.bounds.width() * 0.08,
+            this.bounds.minY + this.bounds.height() * 0.08, 
+            this.bounds.maxX - this.bounds.width() * 0.08,
+            this.bounds.maxY - this.bounds.height() * 0.08
+            ),
+        highpoints = [];
+        while(irr--)
+        {
+            highpoints.push(new Highpoint(
+                random.randomRange(bounds.minX,bounds.maxX),
+                random.randomRange(bounds.minY,bounds.maxY),
+                random.randomRange(this.props["X Range"][0],this.props["X Range"][1]),
+                random.randomRange(this.props["Y Range"][0],this.props["Y Range"][1]),
+                random.randomRange(this.props["Elevation Range"][0],this.props["Elevation Range"][1]),
+                random.randomRange(this.props["Elevation Dropoff"][0],this.props["Elevation Dropoff"][1]),
+            ));
+        }
+        this.graph.highpoints = highpoints;
+        return false;
     }
     LandBuilding()
     {
-       return false;
+        let CenterWeight = this.props["Center Weight"][1], PerlinWeight = this.props["Perlin Weight"][1], WaterHeight = this.props["Water Height"];
+        let t = Math.max(CenterWeight + PerlinWeight, 0.00000001) //so we dont div by 0;
+        CenterWeight /= t, PerlinWeight /= t; //set up weights
+
+        //get our max number of nodes over all irrations
+        this.dataStack.num = this.dataStack.num ? this.dataStack.num : this.graph.corners.length;
+        let max = 1000;
+        let i = this.dataStack.num, random = this.Seeds.Var, elevation, chance;
+        const calculatefromhighpoints = (hps, point) => //little helper function set to const so we dont have to worry about defining it each time
+        {
+            let r, d, n, x=0, b, y=0, total = 0;
+            let index = -1, leastdist = Infinity, // set this to infinity so nothing can match it
+            dist, iHigh = hps.length, pos, p = new Vec2(point.x, point.y),h;
+            while(iHigh--)
+            {
+                h = hps[iHigh];
+                pos = h.position;
+                dist = p.dist(pos); //kinda hate that we dont have type casting for these reasons
+                console.log(dist);
+                leastdist = h.size.y > h.size.x ? h.size.y : h.size.x; //miniumdistance that we should even consider calculating (this is where this highpoint would give 0.0 < to the elevation)
+                if(dist < leastdist && random.randomRange(0,5) >= this.props["Coastal Roughness"][1]) // add in some random chance that we assign a lower elevation so we get more islands around our islands
+                {
+                    // here we are gonna deviate from the original code and base our stuff on all highpoints rather than one this should give us better results
+                    /*f\left(x\right)=\left|\frac{8r}{x}\left|\sin\left(\frac{d\cdot1000}{x}\right)\right|\ +\frac{2^{b}}{\left(x\right)}\right|*/
+                    //f(x) = |(8r)/x * |sin(30,000/x)| + (2^b)/x|
+                    //where y = f(x) > 1.5 ? 1.5 : f(x); and undefined outside the max range
+                    //formula in which we can use an range offset and make x the dist between the point and the hp + random.range which will give us a controled random
+
+                    //calculate the x dist and y dist seperately as each has a different data set
+                    x = Math.max(Math.abs(p.x-pos.x) - random.randomRange(0,5), 0.001); //this function is not defined at 0 so we dont wanna get NaN, add some random offset to give us some random ocollation
+                    y = Math.max(Math.abs(p.y-pos.y) - random.randomRange(0,5), 0.001);
+                    
+                    n = new Vec2(x,y).normalize();
+                    b = h.dispation;
+                    r = h.range;
+                    d = h.occolation;
+                    
+                    //set x and y to the new value assigned by this formula make sure we are still in the domain of the highpoint;
+                    if(h.size.x >= x) x = Math.abs(((8*r)/x) * Math.abs(Math.sin((d*1000)/x)) + (Math.pow(2,b)/x))*h.elevation*Math.abs(n.x); else x =0;
+                    if(h.size.y >= y) y = Math.abs(((8*r)/y) * Math.abs(Math.sin((d*1000)/y)) + (Math.pow(2,b)/y))*h.elevation*Math.abs(n.y); else y=0;
+                    total += x+y;
+                }
+            }
+            console.log(total,x,y,"total")
+            return total;
+        }
+        //base setup of variables
+        let q, pos, neighbors;
+
+        while( i-- && max--) // basically go max times unless this.dataStack.num is less
+        {
+            q = this.graph.corners[i];
+            //console.log(q.HasTerrianValue());
+            if(false) continue; //if we have already processed this value then we want to skip it. this is mainly for corners
+            if(q.IsBoundary(this.bounds)) //if we on the edge we want to be ocean so no more calc needed
+            {
+                //handle map edges
+                //neighbors = q.neighbors;
+                q.SetTerrianProperties(0,1,TerrainType.OCEAN);
+                this.graph.OceanCorners.push(q);
+                q.touches.forEach(x => x.SetCornerAverage()); //go to each corner this guy touches and recalc the data
+                continue;
+            }
+
+            elevation = (random.random() * PerlinWeight) + (calculatefromhighpoints(this.graph.highpoints, q.position) * CenterWeight);
+
+            if(elevation < WaterHeight)
+            {
+                neighbors = q.neighbors;
+                chance = 0;
+                for (let j = 0; j < neighbors.Count; j++)
+                {//basically set up a random chance based on surounding Terrain
+                    if (neighbors[j].terrian == TerrianType.LAND) chance += 3; 
+                    else if (neighbors[j].terrian == TerrianType.LAKE) chance--; 
+                    else if (neighbors[j].terrian == TerrianType.OCEAN) chance++; //add a chance that land will clump a lil
+                    else ;//unprocessed do nothing
+                }
+                //this will be water
+                if(random.randomRange(0, 100*chance)*0.01 < 1.5)
+                {
+                    q.SetTerrianProperties(elevation,1.0, TerrainType.LAKE); //keep in mind lake will be ocean if can reach edge of map
+                    this.graph.LakeCorners.push(q);
+                    q.touches.forEach(x => x.SetCornerAverage()); //update cells
+                    continue; //exit here 
+                }
+            }
+            q.SetTerrianProperties(elevation, 0.0, TerrainType.LAND);
+            this.graph.LandCorners.push(q);
+            q.touches.forEach(x => x.SetCornerAverage());
+        }
+
+        this.dataStack.num = i;
+        return this.dataStack.num >= 0;
     }
     CoastalCleanup()
     {
@@ -310,13 +443,14 @@ class RandomMap
     {
        return false;
     }
-    TempatureRoughCover()
+    TempatureRoughCover() //starting here we will use evolutionary seed
     {
        return false;
     }
     WaterSheding()
     {
         this.shouldRender = false;
+        let ctx = this.canvas.getContext('2d');
         return true;
     }
 }
@@ -336,7 +470,7 @@ if(rndContainor)
         {
             Map.GenerateMap(canvas);
         }
-}, 100);
+}, 10);
 else
     console.error("could not mount canvas to dom, this could be due to browser incompatiblity");
 
